37112800b4b3fbe13a5dc0ef8743c2064c27a0ec
Cogiii <laurencedevera8@gmail.com>
2025-03-04 20:57:54 +0800

    feat: 404 url handling

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,18 @@ const path = require('path');
 
 const router = express.Router();
 
+// Define routes
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../../client/public/index.html"));
+    res.sendFile(path.join(__dirname, '../../client/public/index.html'));
+});
+
+router.get('/appointment', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/public/appointment/index.html'));
+});
+
+// 404 Not Found Handler (should be at the end)
+router.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '../../client/public/404.html'));
 });
 
 module.exports = router;
